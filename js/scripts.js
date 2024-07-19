6bcbe49ffd2e5cc82ffd45d3918511c88ddb7ce8
lifelonglearnerPM <133527988+lifelonglearnerPM@users.noreply.github.com>
2024-07-19 23:09:27 +0530

    improvements in scripts.js
    improvements

--- js/scripts.js
+++ js/scripts.js
@@ -2,13 +2,10 @@ document.addEventListener('DOMContentLoaded', function() {
     const sections = document.querySelectorAll('section');
     const navLinks = document.querySelectorAll('nav ul li a');
 
-    function makeActive() {
+    function makeActive(event) {
+        event.preventDefault();
         const sectionId = this.getAttribute('href').substring(1);
-        sections.forEach(section => {
-            if (section.id === sectionId) {
-                section.scrollIntoView({ behavior: 'smooth' });
-            }
-        });
+        document.getElementById(sectionId).scrollIntoView({ behavior: 'smooth' });
 
         navLinks.forEach(link => link.classList.remove('current'));
         this.classList.add('current');
